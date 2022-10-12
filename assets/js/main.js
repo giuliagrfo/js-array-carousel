ca8c71d7a00abcaa75c240ee4a879319db9b0ffe
giuliagrfo <giuliagrifo1@gmail.com>
2022-10-12 15:59:21 +0200

    Array - loop

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,17 @@ const slides = [
 './assets/img/04.webp',
 './assets/img/05.webp'
 ]
+// stampo in console array 
+console.log(slides);
+
+
+// seleziono elemento della DOM dove inserire le immagini
+const slidesElement = document.querySelector('.slides');
+
+// scorro le immagini nell'array e le inserisco dinamicamente nell'elemento della DOM
+for (let i = 0; i < slides.length; i++) {
+    const slideImg = slides[i];
+    const slideToCreate = `<img class="active" src="${slideImg}" alt="">`;
+    slidesElement.insertAdjacentHTML('beforeend', slideToCreate);
+    console.log(slideToCreate);
+}
